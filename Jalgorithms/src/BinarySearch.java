cefffa4f6ca8c1d5d0c5306978ba999c1523c865
Andrewpqc <m13636038496@163.com>
2018-10-14 09:38:26 +0800

    update

--- Jalgorithms/src/BinarySearch.java
+++ Jalgorithms/src/BinarySearch.java
@@ -1,25 +1,39 @@
 // 二分搜索
 
 import java.util.Arrays; //Arrays.sort()
+import java.util.Collections;
 
 public class BinarySearch {
-    public static int rank(int[] arr, int key) {
-        Arrays.sort(arr);
+    public static int rank1(int[] arr, int key) {
         int l = 0;
         int h = arr.length - 1;
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            if (key > arr[mid]) h = mid - 1;
-            else if (key < arr[mid]) l = mid + 1;
-            else return mid;
+            if (key < arr[mid])      h = mid - 1;
+            else if (key > arr[mid]) l = mid + 1;
+            else                     return mid;
         }
+        return -1;
+    }
+
+    public static int rank2(int[] arr, int l, int h, int key) {
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (key < arr[mid])      return rank2(arr, l, mid - 1, key);
+            else if (key > arr[mid]) return rank2(arr, mid + 1, h, key);
+            else                     return mid;
+        }
+
         return -1;
     }
 
     public static void main(String[] args) {
         int[] myarr = {1, 5, 2, 3, 6, 8};
-        int key = 9;
-        int result = rank(myarr, key);
-        System.out.println(result);
+        Arrays.sort(myarr);  //从小到大排序
+        int key = 2;
+        int result1 = rank1(myarr, key);
+        int result2 = rank2(myarr, 0, myarr.length - 1, key);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }

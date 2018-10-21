c9d661e3700feacffe187ccb72ee6b756fdb53ad
Andrewpqc <m13636038496@163.com>
2018-10-21 09:57:20 +0800

    feat: add Cycle and TwoColor

--- Jalgorithms/src/graph/Cycle.java
+++ Jalgorithms/src/graph/Cycle.java
@@ -4,37 +4,37 @@
 package graph;
 
 public class Cycle {
-    private boolean [] marked;
+    private boolean[] marked;
     private boolean hasCycle;
 
-    public Cycle(Graph g){
+    public Cycle(Graph g) {
         marked = new boolean[g.V()];
-        for(int s=0;s<g.V();++s){
-            if(!marked[s]){
-                dfs(g,s,s);
+        for (int s = 0; s < g.V(); ++s) {
+            if (!marked[s]) {
+                dfs(g, s, s);
             }
         }
     }
 
-    private void dfs(Graph g, int x, int y){
-        marked[x]=true;
-        for(int w: g.adj(x))
-            if(!marked[w]) dfs(g,w,x);
-            else if(w!=y) hasCycle =true;
+    private void dfs(Graph g, int x, int y) {
+        marked[x] = true;
+        for (int w : g.adj(x))
+            if (!marked[w]) dfs(g, w, x);
+            else if (w != y) hasCycle = true;
     }
 
-    public boolean isHasCycle(){
+    public boolean isHasCycle() {
         return hasCycle;
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Graph g = new Graph(8);
         g.addEdge(0, 1);
         g.addEdge(1, 2);
         g.addEdge(2, 3);
         g.addEdge(3, 4);
-        g.addEdge(4,0);
+        g.addEdge(4, 0);
         g.addEdge(5, 6);
         g.addEdge(6, 7);
 
---
+++ Jalgorithms/src/graph/TwoColor.java
@@ -0,0 +1,53 @@
+/**
+ * 检测一个图是否为二分图
+ */
+
+package graph;
+
+
+public class TwoColor {
+    private boolean[] marked;
+    private boolean[] color;
+    private boolean isTwoColor = true;
+
+
+    public TwoColor(Graph g){
+        marked = new boolean[g.V()];
+        color = new boolean[g.V()];
+        for(int i=0;i<g.V();++i){
+            if(!marked[i]){
+                dfs(g,i);
+            }
+        }
+    }
+
+    private void dfs(Graph g, int s){
+        marked[s]=true;
+        for(Integer k : g.adj(s)){
+            if(!marked[k]){
+                color[k]=!color[s];
+                dfs(g,k);
+            }
+            else if(color[k]==color[s]) isTwoColor=false;
+        }
+    }
+
+    public boolean isTwoColor() {
+        return isTwoColor;
+    }
+
+
+    public static void main(String[] args) {
+        Graph g = new Graph(8);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 0);
+        g.addEdge(5, 6);
+        g.addEdge(6, 7);
+
+        TwoColor tc = new TwoColor(g);
+        System.out.println(tc.isTwoColor());
+    }
+}
